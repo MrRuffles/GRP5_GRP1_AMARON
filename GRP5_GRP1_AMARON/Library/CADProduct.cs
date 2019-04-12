3036c8647f6ff698653c357d4ec3c039e73618ef
Tamara Salcedo <tamarauni2016@gmail.com>
2019-04-12 19:47:45 +0200

    Created classes ENProduct and CADProduct.

---
+++ GRP5_GRP1_AMARON/Library/CADProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class CADProduc { 
+
+    //Initializates connection string to data  base
+    public CADProduct() { }
+
+    /*
+     * Updates the product in the DataBase
+     * Parameters: product to update
+     * Return: true in case that the product could be updated
+    */
+    public bool UpdateProduct(ENProduct product){
+        bool updated = false;
+
+        return updated;
+    }
+
+}//End class CADProduct
---
+++ GRP5_GRP1_AMARON/Library/ENProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class ENProduct{
+
+    //Properties------------------------------------------------------------------------------------
+
+    private string ProductName;
+    public string name{
+
+        get { return this.ProductName;  }
+
+        set { this.ProductName = value; }
+
+    }
+
+    private float ProductPrice;
+    public float price{
+
+        get { return this.ProductPrice; }
+
+        set { this.ProductPrice = value; }
+
+    }
+
+    private string ProductDescription;
+    public string description {
+
+        get { return this.ProductPrice;  }
+
+        set { this.ProductDescription = value; }
+
+    }
+
+
+    //----------------------------------------------------------------------------------------------
+
+    //Creates a default product
+	public ENProduct(){
+
+        name = "Default Name";
+        price = 0.0;
+                
+
+	}
+
+}//fin class ENProduct
