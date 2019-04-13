ea44b64723bc69183573a119d82f4d478695850d
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2019-04-13 12:07:24 +0200

    Created CAD/EN for Cart

---
+++ GRP5_GRP1_AMARON/Library/CADCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+public class CADCart
+{
+	public CADCart()
+	{
+	}
+
+    public bool CreateCart(ENCart cart)
+    {
+        bool created = false;
+
+        return created;
+    }
+}
---
+++ GRP5_GRP1_AMARON/Library/ENCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ENCart
+    {
+        private float CartSum;
+        public float sum
+        {
+            get { return CartSum; }
+            set { CartSum = value; }
+        }
+
+        public ENCart()
+        {
+            sum = 0;
+        }
+    }
+}
