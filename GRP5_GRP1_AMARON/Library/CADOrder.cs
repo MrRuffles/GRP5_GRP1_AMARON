2e334a1928f1f2d982c2d8588ea71afd8ff20b7b
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2019-04-13 12:07:57 +0200

    Created CAD/EN for Order

---
+++ GRP5_GRP1_AMARON/Library/CADOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Library
+{
+
+    public class CADOrder
+    {
+        public CADOrder()
+        {
+        }
+
+
+        public bool CreateOrder(ENOrder order)
+        {
+            bool updated = false;
+
+            return updated;
+        }
+    }
+}
---
+++ GRP5_GRP1_AMARON/Library/ENOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ENOrder
+    {
+
+        private string OrderState;
+        public string state
+        {
+            get { return OrderState; }
+            set { OrderState = value; }
+        }
+
+        private float OrderCost;
+        public float cost
+        {
+            get { return OrderCost; }
+            set { OrderCost = value; }
+        }
+
+        private string OrderDate;
+        public string date
+        {
+            get { return OrderDate; }
+            set { OrderDate = value; }
+        }
+
+        public ENOrder()
+        {
+            state = "";
+            cost = 0;
+            date = "0/0/0";
+        }
+    }
+}
