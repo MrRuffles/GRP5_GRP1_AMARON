cbe5eae2548cd397f38f1a1f127c32b7f9b64d23
Sam <sac52@alu.ua.es>
2019-04-14 04:29:09 +0200

    74444175Y - Added EN/CAD for Suggest and Support

---
+++ GRP5_GRP1_AMARON/Library/CADSupport.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Library
+{
+    public class CADSupport
+    {
+        private string constring;
+
+        public CADSupport()
+        {
+            constring = ConfigurationManager.ConnectionStrings["conex"].ConnectionString;
+        }
+
+        public bool createSupport(ENSupport en)
+        {
+           
+            return true;
+
+        }
+
+        public bool readSupport(ENSupport en)
+        {
+            return true;
+
+        }
+
+        public bool updateSupport(ENSupport en)
+        {
+            return true;
+
+        }
+
+        public bool deleteSupport(ENSupport en)
+        {
+
+            return true;
+        }
+    }
+}
