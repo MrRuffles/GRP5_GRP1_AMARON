9f9396078a768c58bf9c66dcb934798b447e3fc0
Paula Guadalajara Saiz <pgs68@alu.ua.es>
2019-04-14 11:53:43 +0200

    04639663B
    Added CADProvider, CADUser, ENProvider and ENUser

---
+++ GRP5_GRP1_AMARON/Library/CADProvider.cs
@@ -0,0 +1,43 @@
+﻿//Paula Guadalajara Saiz
+
+using System;
+
+namespace Library
+{
+
+    public class CADProvider
+    {
+
+        private string constring;
+
+        /**  Initializes connection string to data base  **/
+        public CADProvider()
+        {
+            constring = ConfigurationManager.ConnectionStrings["conex"].ConnectionString;
+        }
+
+        /**  Adds a new provider to data base  **/
+        public bool CreateProvider(ENProvider provider)
+        {
+            return false;
+        }
+
+        /**  Reads a provider from data base  **/
+        public bool ReadProvider(ENProvider provider)
+        {
+            return false;
+        }
+
+        /** Updates a provider from data base **/
+        public bool UpdateProvider(ENProvider provider)
+        {
+            return false;
+        }
+
+        /**  Deletes a provider from data base  **/
+        public bool DeleteProvider(ENProvider provider)
+        {
+            return false;
+        }
+    }
+}
