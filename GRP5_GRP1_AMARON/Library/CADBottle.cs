1c65ca54e454a91712223ce02d6d9148d42c0ae1
Tamara Salcedo <tamarauni2016@gmail.com>
2019-04-14 12:29:57 +0200

    75944876A
    Created classes CADBottle and ENBottle, CRUD's methods in progress

---
+++ GRP5_GRP1_AMARON/Library/CADBottle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Library;
+
+
+namespace Library{
+
+      public class CADBottle{
+       
+            //Initializates connection string to data base
+            public CADBottle() { }
+
+            /*
+             * Adds a product to the Data Base
+             * Paramaters: product to create
+             * Returns: true if the product could be created, false on the contrary
+             */
+            public bool CreateBottle(ENBottle bottle)
+            {
+
+                bool created = false;
+
+                return created;
+
+            }
+
+            /*
+             *Reads a bottle from Data Base
+             * Parameters: bottle to read
+             * Returns: true if the bottle could be read, false on the contrary
+             */
+            public bool ReadBottle(ENBottle bottle){
+
+                bool read = false;
+
+                return read;
+
+            }
+
+            /*
+             * Updates the bottle in the DataBase
+             * Parameters: bottle to update
+             * Return: true in case that the bottle could be updated, false on the contrary
+            */
+            public bool UpdateBottle(ENBottle bottle){
+
+                bool updated = false;
+
+                return updated;
+            }
+
+            /*
+             * Deletes the bottle in the Data Base
+             * Parameters: product to delete
+             * Return: true in case that the product could be deleted, false on the contrary
+            */
+            public bool DeleteProduct(ENProduct product)
+            {
+
+                bool deleted = false;
+
+                return deleted;
+
+            }
+
+        }
+    }
+
+
+}
+
